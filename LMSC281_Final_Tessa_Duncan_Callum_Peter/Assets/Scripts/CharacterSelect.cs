0c01446cb739f9e545a70fe6055c943ccceccd4e
Peterooooo <32879951+Peterooooo@users.noreply.github.com>
2017-12-02 19:19:57 -0500

    Scriptable Objects

---
+++ LMSC281_Final_Tessa_Duncan_Callum_Peter/Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Character")]
+public class CharacterSelect : ScriptableObject {
+
+	public string characterName = "Default";
+	public int Strength = 1;
+	public int Intelligence = 1;
+	public int Agility = 1;
+	public int Magic = 1;
+}
+
