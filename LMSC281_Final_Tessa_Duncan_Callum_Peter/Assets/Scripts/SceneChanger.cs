f90db2190a0c9863e9f9cd31470e0b49e8188e20
DuncanAnderson <32934485+DuncanAnderson@users.noreply.github.com>
2017-12-02 19:42:32 -0500

    Main Menu

--- LMSC281_Final_Tessa_Duncan_Callum_Peter/Assets/Scripts/SceneChanger.cs
+++ LMSC281_Final_Tessa_Duncan_Callum_Peter/Assets/Scripts/SceneChanger.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour
-{
-<<<<<<< HEAD
-	public void changeScene ()
-=======
-	public void LoadScene()
->>>>>>> b09f684a8954dc60cbaaf59409c6d57a2a79f404
-	{
-		SceneManager.LoadScene (1); 
+public class SceneChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
 }
