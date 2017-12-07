9b6ac26ada1fb6c209c75cd15ff354b6b5f653e9
Peterooooo <32879951+Peterooooo@users.noreply.github.com>
2017-12-06 22:24:40 -0500

    beer

---
+++ LMSC281_Final_Tessa_Duncan_Callum_Peter/Assets/Scripts/CharacterClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	[CreateAssetMenu (menuName = "Character Class")]
+	public class CharacterClass : ScriptableObject {
+
+		public string characterName = "Default";
+		public int Strength = 1;
+		public int Intelligence = 1;
+		public int Agility = 1;
+		public int Magic = 1;
+	}
